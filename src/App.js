9e85cd6b4add383c3e0677a65277306bb38ab5a5
Dixit <therishabhdixit01@gmail.com>
2022-05-18 10:07:02 +0530

    functional component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import React, { Component } from 'react';
 import Header from './MyComponent/Header';
+import Footer from './MyComponent/Footer';
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Footer/>
     </div>
   );
 }
--- src/MyComponent/Header.js
+++ src/MyComponent/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Welcome to my first Component</div>
+    <div className='header'>Welcome to my first Component</div>
   )
 }
 
